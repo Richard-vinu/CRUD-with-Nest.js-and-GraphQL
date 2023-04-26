c86c073f21e09008fffe10abe62aefc175f60359
Richard_it <78643671+Richard-vinu@users.noreply.github.com>
2023-04-26 16:29:42 +0530

    Update database.ts

--- database.ts
+++ database.ts
@@ -7,6 +7,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
+POSTGRES_HOST=127.0.0.1
+POSTGRES_PORT=5432
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=richard
+POSTGRES_DATABASE=linkedin
+JWT_SECRET=zysk
+
 @Module({
   imports: [
    ConfigModule.forRoot({ isGlobal: true }),
